9d6eb0b729141f906293e5bfd50dcf8b6c5071ec
Juan Fernando Moreno <juanfernandomoreno04@gmail.com>
2024-05-28 23:54:17 -0500

    Corrections on mongo url

--- dist/config/index.js
+++ dist/config/index.js
@@ -1,7 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.TWILIO_AUTH_TOKEN = exports.TWILIO_ACCOUNT_SID = exports.PORT = exports.APP_SECRET = exports.MONGO_URL = void 0;
-exports.MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017/online_food_delivery';
+exports.MONGO_URL = process.env.MONGO_URL || 'mongodb+srv://juanfernandomoreno04:wEsQ5sdTVxjE2U6@cluster0.thmyt6p.mongodb.net/online_food_delivery?retryWrites=true&w=majority&appName=Cluster0';
+//  'mongodb://localhost:27017/online_food_delivery';
 exports.APP_SECRET = process.env.APP_SECRET || 'some_very_secret_key';
 exports.PORT = process.env.PORT || 8000;
 exports.TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID;
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,4 @@
-export const MONGO_URL = process.env.MONGO_URL || 'mongodb+srv://juanfernandomoreno04:<password>@cluster0.thmyt6p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+export const MONGO_URL = process.env.MONGO_URL || 'mongodb+srv://juanfernandomoreno04:wEsQ5sdTVxjE2U6@cluster0.thmyt6p.mongodb.net/online_food_delivery?retryWrites=true&w=majority&appName=Cluster0';
 //  'mongodb://localhost:27017/online_food_delivery';
 export const APP_SECRET = process.env.APP_SECRET || 'some_very_secret_key'
 export const PORT = process.env.PORT || 8000
